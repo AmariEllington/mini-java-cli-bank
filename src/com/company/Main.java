ac2c33e39408f973447f6dbb6ce47ccd68022816
Amari Ellington <amari.l.ellington@gmail.com>
2020-10-21 17:27:47 +0100

    finished cli bank app

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Account newUser = new Account("User Name", "0023443");
+        newUser.showMenu();
+    }
+}
